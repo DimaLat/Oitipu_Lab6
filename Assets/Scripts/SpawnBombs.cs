0584958031167fccadb1dac5be693f0e0c66c99d
DimaLat <dima.latyshionok@gmail.com>
2020-12-13 21:31:38 +0300

    init commit

---
+++ Assets/Scripts/SpawnBombs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class SpawnBombs : MonoBehaviour
+{
+
+    public GameObject bomb;
+
+
+    void Start()
+    {
+        StartCoroutine(Spawn());    
+    }
+    IEnumerator Spawn()
+    {
+        while (!Player.lose)
+        {
+            Instantiate(bomb, new Vector2(Random.Range(-0.38f, 4), 5.1f), Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
