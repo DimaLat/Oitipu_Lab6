6e052c6b9720cdb098500f077732dc79ea9c412a
DimaLat <dima.latyshionok@gmail.com>
2020-12-15 20:40:42 +0300

    add movement of players sprite from one side of the screen to the opposite

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -11,11 +11,15 @@ public class MovePlayer : MonoBehaviour
     {
         if (!Player.lose) { 
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.x = mousePos.x > 4.0f ? 4.0f : mousePos.x;
-            mousePos.x = mousePos.x < -0.38f ? -0.38f : mousePos.x;
+            if (player.position.x < -1)
+                player.position = new Vector3(player.position.x + 10f, player.position.y, player.position.z);
+            else if (player.position.x > 5)
+                player.position = new Vector3(player.position.x - 10f, player.position.y, player.position.z);
+
             player.position = Vector2.MoveTowards(player.position, 
                 new Vector2(mousePos.x, player.position.y),
                 speed * Time.deltaTime);
     }
     }
+
 }
